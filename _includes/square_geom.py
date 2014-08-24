20d4de3b88135775918ed8037f4002bcfb717f16
Carl Pearson <cap10@ufl.edu>
2014-08-24 08:56:57 -0400

    updates to square_geom

--- _includes/square_geom.py
+++ _includes/square_geom.py
@@ -1,6 +1,6 @@
 def square_perimeter(side):
     """
-    'square_perimeter' calculates a square's perimeter given a side length, e.g.:
+    'square_perimeter' calculates a square's perimeter given a side length:
     >>> square_perimeter(4)
     16
 
@@ -11,8 +11,19 @@ def square_perimeter(side):
 
 
 def square_area(side):
+    """
+    'square_area' calculates a square's area given a side length:
+    >>> square_perimeter(4)
+    16
+
+    :param side: the side length
+    :return: the area (units^2 from side)
+    """
     return side*side
 
 if __name__ == "__main__":
     sampleSide = 4
-    print("area:", square_area(sampleSide),"perimeter:", square_perimeter(sampleSide))
+    print("area:",
+          square_area(sampleSide),
+          "perimeter:",
+          square_perimeter(sampleSide))
