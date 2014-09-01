52959cfad71b67e99917afc4e011932b9fc9c3b6
Carl Pearson <cap10@ufl.edu>
2014-09-01 05:33:25 -0400

    update turtle example to take arguments

--- _includes/plot_ii.py
+++ _includes/plot_ii.py
@@ -1,11 +1,10 @@
 import turtle
 
-side = 50
 
-i = 0;
-while (i < 4):
-    turtle.forward(side)
-    turtle.left(90)
-    i = i+1
-
-turtle.done()
+def draw_square(side):
+    i = 0
+    while (i < 4):
+        turtle.forward(side)
+        turtle.left(90)
+        i = i+1
+    turtle.done()
