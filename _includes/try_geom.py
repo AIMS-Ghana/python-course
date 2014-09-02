6dc464e539defeefaa03b9f210f03d1905ff2a42
Carl Pearson <cap10@ufl.edu>
2014-09-02 03:46:51 -0400

    tweak to add `as` syntax

--- _includes/try_geom.py
+++ _includes/try_geom.py
@@ -5,8 +5,8 @@ from square_geom_with_validation import *
 def square_try(n=0):
     try: # steps you think might produce an error
         s = float(input("Provide a square side:"))
-    except ValueError: # how you want to handle that error
-        print("You did not enter a number, try again.")
+    except ValueError as err: # how you want to handle that error
+        print("You did not enter a number, try again. Error:", err)
         square_try(n+1)
     else: # what to do if there was no error
         print("Square perimeter is:", square_perimeter(s))
