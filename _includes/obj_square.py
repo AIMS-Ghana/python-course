a8167ee435613b11495c88ea6c90218f9f8b07b6
Carl Pearson <cap10@ufl.edu>
2014-09-03 10:55:59 -0400

    mostly there on square object

--- _includes/obj_square.py
+++ _includes/obj_square.py
@@ -1,4 +1,29 @@
 __author__ = 'cap10'
 
+import turtle
+
 class Square(object):
-    pass
+    """a class representation of square"""
+    squares_created = 0
+    def __init__(self, side_length:(int,float)):
+        self.squares_created += 1
+        self.side_length = side_length
+
+    def __str__(self):
+        return "Square, side length: "+str(self.side_length)
+
+    def area(self):
+        return self.side_length**2
+
+    def perimeter(self):
+        return self.side_length*4
+
+    def draw(self):
+        for i in range(0,4):
+            turtle.forward(self.side_length)
+            turtle.left(90)
+        turtle.done()
+
+sq = Square(50)
+print(sq,sq.area(),sq.perimeter())
+sq.draw()
